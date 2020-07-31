36aa5aaa0da9dd5e1078d665b39b6ef6aa1385cc
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-31 11:52:52 -0500

    scope para negocios activos

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -3,7 +3,13 @@ require 'active_record/diff'
 class Deal < ApplicationRecord
   include ActiveRecord::Diff
   diff exclude: [:id, :created_at, :updated_at, :change_flag, :trello_flag]
+
+  belongs_to :legal_state
+
+  scope :active, -> { joins(:legal_state).where.not(legal_states: {name: "VENDIDA"})
+                                         .where.not(legal_states: {name: "ENTREGA DE VIVIENDA"})
+                                         .where.not(legal_states: {name: "DESEMBOLSADO"})
+                                         .where.not(legal_states: {name: "LIBRE"})
+                                       }
 end
-
-
 
