0e4baf63c91ead1b58fc7c73e32e4ff17e3a19db
Daniel Lopez <dlopez.10g@gmail.com>
2020-07-31 11:52:12 -0500

    filtro de deals por pendientes para subir a trello

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,4 +1,7 @@
 class DealsController < ApplicationController
   def index
+    @deals = Deal.where(change_flag: true)
+                 .or(Deal.where.not(trello_flag: true))
+                 .active
   end
 end
