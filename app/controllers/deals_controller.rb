5e03e6a1b7389d0d7c7c6986a8d81d5789c105d5
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-04 18:47:23 -0500

    call class method to iterate over deals without n + 1 queries

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,10 @@
 class DealsController < ApplicationController
   def index
     @deals = Deal.where(change_flag: true)
-                 .or(Deal.where.not(trello_flag: true))
+                 or(Deal.where.not(trello_flag: true))
                  .active
+                 .with_board
+    @deals = @deals.includes(:legal_state).limit(5)
+    Deal.iterate_over_deals(@deals)
   end
 end
