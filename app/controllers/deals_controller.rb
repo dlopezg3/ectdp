eabba019923f420394968fd2df6ec7679c902efc
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-04 18:51:12 -0500

    bug on deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,7 +1,7 @@
 class DealsController < ApplicationController
   def index
     @deals = Deal.where(change_flag: true)
-                 or(Deal.where.not(trello_flag: true))
+                 .or(Deal.where.not(trello_flag: true))
                  .active
                  .with_board
     @deals = @deals.includes(:legal_state).limit(5)
