ce3a9051940d523975634c7821415e1c45321ad1
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-06 16:06:27 -0500

    a legal state can have many durations according to its variables

--- app/models/legal_state.rb
+++ app/models/legal_state.rb
@@ -2,4 +2,5 @@ class LegalState < ApplicationRecord
   has_many :deals
   has_many :bank_labels, dependent: :destroy
   has_many :subsidy_labels, dependent: :destroy
+  has_many :legal_state_durations
 end
