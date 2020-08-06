d264d5a725417dcf1087b48260dd2cbbb4fba404
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-06 16:06:38 -0500

    eager load on lib files

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module Ectdp
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
