f7a6dcc7fc9ac55fe7a750e025441780628c6246
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-14 18:45:53 -0500

    cambios en board y lists ids

--- db/seeds/legal_state.rb
+++ db/seeds/legal_state.rb
@@ -2,60 +2,61 @@ Deal.destroy_all
 LegalState.destroy_all
 
 legal_states = [
-                { name: "A DESISTIR", board_tid: ""},
-                { name: "A REUBICAR", board_tid: ""},
-                { name: "ACLARATORIA AL SUBSIDIO", board_tid: ""},
-                { name: "APROBADO ", board_tid: ""},
-                { name: "AVALUO FINAL", board_tid: "5f1ef0101d91da3d6fb414dc", grouped: true},
-                { name: "AVALUO PRELIMINAR", board_tid: "5f1ef0101d91da3d6fb414dc", grouped: true},
-                { name: "CARTA DE NOVEDAD", board_tid: ""},
-                { name: "CARTA DE RATIFICACION ", board_tid: ""},
-                { name: "CASO INMOBILIARIA", board_tid: ""},
-                { name: "DESEMBOLSADO", board_tid: ""},
-                { name: "ELAB ESCRITURACION", board_tid: "5f1ef05b5cd4c8412a10ab88"},
-                { name: "EN ESPERA DE RESOLUCION MCY (PROCESO 027)", board_tid: ""},
-                { name: "ENTRADA DE REGISTRO", board_tid: "5f1ef06ed5c05923805fbda4", grouped: true},
-                { name: "ENTREGA DE VIVIENDA", board_tid: ""},
-                { name: "ESTUDIO TITULO (PROCESO 027)", board_tid: ""},
-                { name: "FIRMA BANCO", board_tid: "5f1857980fbd968e3cff4b08", grouped: true},
-                { name: "FIRMA BANCO CONSTRUCTOR", board_tid: "5f1857980fbd968e3cff4b08", grouped: true},
-                { name: "FIRMA ESC. CLIENTE", board_tid: "5f1857980fbd968e3cff4b08", grouped: true},
-                { name: "FIRMA FIDUCIA", board_tid: "5f1857980fbd968e3cff4b08", grouped: true},
-                { name: "LIBRE", board_tid: ""},
-                { name: "LIBRE INVERSION", board_tid: ""},
-                { name: "LIQUIDACION", board_tid: ""},
-                { name: "NEGADO", board_tid: ""},
-                { name: "PREAPROB Y CONSEC", board_tid: ""},
-                { name: "PROTOCOLIZACION", board_tid: "5f1ef15e8abfd186d989fcd2"},
-                { name: "RADICADO EN ESPERA DE DESEMBOLSO", board_tid: ""},
-                { name: "REVISION FIDUCIA", board_tid: ""},
-                { name: "SALDO EN EFECTIVO", board_tid: ""},
-                { name: "SALIDA DE REGISTRO", board_tid: "5f1ef06ed5c05923805fbda4", grouped: true},
-                { name: "SUBSIDIO RADICADO", board_tid: ""},
-                { name: "TRAMITE", board_tid: ""},
-                { name: "TRAMITE - ACTUALIZACION", board_tid: ""},
-                { name: "FIRMA ESC. CONSTRUCTORA", board_tid: "5f1857980fbd968e3cff4b08"},
-                { name: "VENDIDA", board_tid: ""}
+                { name: "A DESISTIR", board_tid: "5f36fec6e8daef3469ac1b1a", list_id:"5f36fef3caacef66835581f6"},
+                { name: "A REUBICAR", board_tid: "5f36fec6e8daef3469ac1b1a", list_id:"5f36fec6e8daef3469ac1b1c"},
+                { name: "ACLARATORIA AL SUBSIDIO", board_tid: "5f371c7f7df57a1f050ba0fb", list_id:"5f371c7f7df57a1f050ba0ff"},
+                { name: "APROBADO ", board_tid: "5f36fd2572173a014e3bc186", list_id:"5f36fd2572173a014e3bc187"},
+                { name: "AVALUO FINAL", board_tid: "5f36fd2572173a014e3bc186", list_id:"5f36fd2572173a014e3bc189"},
+                { name: "AVALUO PRELIMINAR", board_tid: "5f36fd2572173a014e3bc186", list_id:"5f36fd2572173a014e3bc188"},
+                { name: "CARTA DE NOVEDAD", board_tid: "5f36fda6193f8676acdd47cd", list_id:"5f36fda6193f8676acdd47ce"},
+                { name: "CARTA DE RATIFICACION ", board_tid: "5f36fde4c9024a470fc8934c", list_id:"5f36fde4c9024a470fc8934d"},
+                { name: "CASO INMOBILIARIA", board_tid: "5f36fec6e8daef3469ac1b1a", list_id:"5f36fec6e8daef3469ac1b1a"},
+                { name: "DESEMBOLSADO", board_tid: "5f371c7f7df57a1f050ba0fb", list_id:"5f371c7f7df57a1f050ba0fc"},
+                { name: "ELAB ESCRITURACION", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f37046a22ad1929d4cdef32"},
+                { name: "EN ESPERA DE RESOLUCION MCY (PROCESO 027)", board_tid: "5f331857625722083bc5d3ea", list_id:"5f3318629447ce1c1cf1490f"},
+                { name: "ENTRADA DE REGISTRO", board_tid: "5f371cebf6497f5db4b73b5f", list_id:"5f371cebf6497f5db4b73b60"},
+                { name: "ENTREGA DE VIVIENDA", board_tid: "5f37006b39980f8c9b4e4aef", list_id:"5f37006b39980f8c9b4e4af0"},
+                { name: "ESTUDIO TITULO (PROCESO 027)", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f332347c7e13a4418c18a91"},
+                { name: "FIRMA BANCO", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f1eefa550e7543f656ccc78"},
+                { name: "FIRMA BANCO CONSTRUCTOR", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f1eef9b20e520012f5bd217"},
+                { name: "FIRMA ESC. CLIENTE", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f185c45782e168d081a7a30"},
+                { name: "FIRMA FIDUCIA", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f1eef9fdfaa3d2441228a85"},
+                { name: "LIBRE", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f1ef05b5cd4c8412a10ab89"},
+                { name: "LIBRE INVERSION", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f1ef05b5cd4c8412a10ab8c"},
+                { name: "LIQUIDACION", board_tid: "5f1ef15e8abfd186d989fcd2", list_id:"5f1ef15e8abfd186d989fcd4"},
+                { name: "NEGADO", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f36fc940b177b2ba3c7726c"},
+                { name: "PREAPROB Y CONSEC", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f1ef05b5cd4c8412a10ab8b"},
+                { name: "PROTOCOLIZACION", board_tid: "5f1ef15e8abfd186d989fcd2", list_id:"5f1ef15e8abfd186d989fcd3"},
+                { name: "RADICADO EN ESPERA DE DESEMBOLSO", board_tid: "5f371cebf6497f5db4b73b5f", list_id:"5f371cebf6497f5db4b73b61"},
+                { name: "REVISION FIDUCIA", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f3700a6a30f89266302ebb6"},
+                { name: "SALDO EN EFECTIVO", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f2accfee570290e693af3f8"},
+                { name: "SALIDA DE REGISTRO", board_tid: "5f371cebf6497f5db4b73b5f", list_id:"5f371cebf6497f5db4b73b62"},
+                { name: "SUBSIDIO RADICADO", board_tid: "5f371c7f7df57a1f050ba0fb", list_id:"5f371c7f7df57a1f050ba0fd"},
+                { name: "TRAMITE", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f1ef05b5cd4c8412a10ab88"},
+                { name: "TRAMITE - ACTUALIZACION", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f36fc909abba8143fd9a207"},
+                { name: "FIRMA ESC. CONSTRUCTORA", board_tid: "5f1857980fbd968e3cff4b08", list_id:"5f2d7b023f782403bb6b0ab2"},
+                { name: "VENDIDA", board_tid: "5f1ef05b5cd4c8412a10ab88", list_id:"5f1ef05b5cd4c8412a10ab8a"},
+                { name: "RADICADO EN BANCO Y CAJA", board_tid: "5f331cd2aa36eb25e0ba2327", list_id:"5f331cdc0aee5a416f58ed9a"},
               ]
 
-def get_board_list(ls, retries = 3)
-  url = "https://api.trello.com/1/boards/#{ls[:board_tid]}/lists?key=#{ENV['TRELLO_KEY']}&token=#{ENV['TRELLO_TOKEN']}"
-  response = HTTParty.get(url)
-  res = JSON.parse(response.body)
+# def get_board_list(ls, retries = 3)
+#   url = "https://api.trello.com/1/boards/#{ls[:board_tid]}/lists?key=#{ENV['TRELLO_KEY']}&token=#{ENV['TRELLO_TOKEN']}"
+#   response = HTTParty.get(url)
+#   res = JSON.parse(response.body)
 
-  if ls[:grouped]
-    return res.select { |board_list| board_list["name"] == ls[:name] }.first["id"]
-  end
-  return res.first["id"]
+#   if ls[:grouped]
+#     return res.select { |board_list| board_list["name"] == ls[:name] }.first["id"]
+#   end
+#   return res.first["id"]
 
-rescue => e
-  puts "TRY #{retries}/n ERROR: timed out while trying to connect #{e}"
-  raise if retries <= 1
-  get_board_list(ls, retries - 1)
-end
+# rescue => e
+#   puts "TRY #{retries}/n ERROR: timed out while trying to connect #{e}"
+#   raise if retries <= 1
+#   get_board_list(ls, retries - 1)
+# end
 
 legal_states.each do |ls|
-  list_id = get_board_list(ls) if !ls[:board_tid].empty?
+  list_id = ls[:list_id]
   puts "Creando #{ls[:name]} - Board Tid: #{ls[:board_tid]}"
   legal_state = LegalState.new(name: ls[:name], board_tid: ls[:board_tid], list_tid: list_id)
   legal_state.save
