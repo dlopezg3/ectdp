10c8850305818eaa6091c6e983511531609e42d9
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-19 15:50:58 -0500

    new method for update cards on trello without update page

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,11 +1,16 @@
 class DealsController < ApplicationController
   def index
-    @deals = Deal.where(change_flag: true)
-                 .or(Deal.where.not(trello_flag: true))
+    @deals = Deal.where(trello_flag: true).limit(10).reverse
+  end
+
+  def update_cards
+    @deals = Deal.unupload
                  .active
-                 .with_board
                  .recent
-    @deals = @deals.includes(:legal_state).limit(5)
+    @deals = @deals.includes(:legal_state).limit(10)
+    @changed_deals = Deal.with_changes
+
     Deal.create_trello_cards(@deals)
+    redirect_to deals_path
   end
 end
