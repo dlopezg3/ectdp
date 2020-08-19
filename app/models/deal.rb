4367f8e8aa129646796e5ae320f0534ecd16ccbb
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-19 15:53:14 -0500

    filters as scopes in deal model

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -11,17 +11,20 @@ class Deal < ApplicationRecord
   belongs_to :legal_state
 
   scope :active, -> { joins(:legal_state).where.not(legal_states: {name: "VENDIDA"})
-                                         .where.not(legal_states: {name: "ENTREGA DE VIVIENDA"})
-                                         .where.not(legal_states: {name: "DESEMBOLSADO"})
                                          .where.not(legal_states: {name: "LIBRE"})
                                        }
-  scope :with_board, -> { joins(:legal_state).where.not(legal_states: {board_tid: ""}) }
   scope :recent, -> { where("legal_state_date > ?", DateTime.parse('01/01/2020').to_date) }
+
+  scope :unupload, -> { where(trello_flag: false) }
+  scope :with_changes, -> { joins(:legal_state).where(change_flag: true)
+                                               .where(legal_states: {name: "PREAPROB Y CONSEC"})
+                                             }
 
   def self.create_trello_cards(deals)
     raise "No se han cargado los estados legales" if LegalState.count == 0
     raise "No se han cargado las labels" if BankLabel.count == 0
     raise "No se han cargado los deals" if Deal.count == 0
+
 
     deals.each do |d|
       next if d.legal_state.board_tid.empty?
