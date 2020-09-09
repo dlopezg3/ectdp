8fdf0cab6e60cc2ad3a825e9f26f544bc0cd15d7
Daniel Lopez <dlopez.10g@gmail.com>
2020-09-09 08:51:39 -0500

    put limits to upload on deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,16 +1,21 @@
 class DealsController < ApplicationController
+  before_action :set_limit, only: %w[index update_cards]
   def index
-    @deals = Deal.where(trello_flag: true).limit(10).reverse
+    @deals = Deal.where(trello_flag: true).sort_by(&:updated_at).reverse[0..(@limit-1)]
   end
 
   def update_cards
     @deals = Deal.unupload
                  .active
                  .recent
-    @deals = @deals.includes(:legal_state).limit(10)
+    @deals = @deals.includes(:legal_state) #.limit(@limit)
     @changed_deals = Deal.with_changes
 
     Deal.create_trello_cards(@deals)
     redirect_to deals_path
+  end
+
+  def set_limit
+    @limit = 50
   end
 end
